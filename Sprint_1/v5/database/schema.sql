c2fc23bb331c08db97ed6ff5aa18d075ddda6b8d
urgyen18 <101525576+urgyen18@users.noreply.github.com>
2025-03-19 22:26:55 -0400

    Update schema.sql

--- Sprint_1/v5/database/schema.sql
+++ Sprint_1/v5/database/schema.sql
@@ -1,6 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ducks (
